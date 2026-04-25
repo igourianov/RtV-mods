0a5303f6ade3b6d49938c34ae46a881ed3f7c5cd
Ilia Gourianov <igourianov@gmail.com>
2026-04-25 16:54:08 -0400

    moved sound file hwere it belongs

--- likhos-weapon-handling-fixes/Scripts/Main.gd
+++ likhos-weapon-handling-fixes/Scripts/Main.gd
@@ -4,8 +4,6 @@ const _Handling = preload("res://mods/likhos-weapon-handling-fixes/Scripts/Handl
 const _WeaponRig = preload("res://mods/likhos-weapon-handling-fixes/Scripts/WeaponRig.gd")
 const _Camera = preload("res://mods/likhos-weapon-handling-fixes/Scripts/Camera.gd")
 
-const _FLASHLIGHT_WAV_PATH = "res://Audio/Interaction/Files/Flashlight.wav"
-
 var _handling
 var _weapon_rig
 var _camera
@@ -17,12 +15,9 @@ func _ready() -> void:
 		return
 	var lib = Engine.get_meta("RTVModLib")
 	var preferences = Preferences.Load()
-	var flashlight_stream = load(_FLASHLIGHT_WAV_PATH)
-	if flashlight_stream == null:
-		push_warning("[likho] failed to load %s" % _FLASHLIGHT_WAV_PATH)
 
 	_weapon_rig = _WeaponRig.new(lib, preferences)
-	_handling = _Handling.new(lib, preferences, flashlight_stream)
+	_handling = _Handling.new(lib, preferences)
 	_camera = _Camera.new(lib, _weapon_rig)
 
 	lib.hook("handling-weaponhandling", _handling.on_weapon_handling)
