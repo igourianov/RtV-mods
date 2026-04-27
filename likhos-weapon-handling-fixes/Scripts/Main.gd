8fd3b998da145d51f51e7f0539374607ecbdf012
Ilia Gourianov <igourianov@gmail.com>
2026-04-26 23:00:27 -0400

    rewrittem hook init

--- likhos-weapon-handling-fixes/Scripts/Main.gd
+++ likhos-weapon-handling-fixes/Scripts/Main.gd
@@ -8,20 +8,47 @@ var _handling
 var _weapon_rig
 var _camera
 
-
 func _ready() -> void:
-	if not Engine.has_meta("RTVModLib"):
+	var lib = Engine.get_meta("RTVModLib")
+	if lib == null:
 		push_error("[likho] RTVModLib meta not available")
 		return
-	var lib = Engine.get_meta("RTVModLib")
+	if lib._is_ready:
+		_init_hooks(lib)
+	else:
+		lib.frameworks_ready.connect(func():
+			_init_hooks(lib)
+		)
+
+func _init_hooks(lib):
 	var preferences = Preferences.Load()
 
 	_weapon_rig = _WeaponRig.new(lib, preferences)
 	_handling = _Handling.new(lib, preferences)
 	_camera = _Camera.new(lib, _weapon_rig)
 
-	lib.hook("handling-weaponhandling", _handling.on_weapon_handling)
-	lib.hook("weaponrig-ammocheck", _weapon_rig.on_ammo_check)
-	lib.hook("weaponrig-ads-post", _weapon_rig.on_ads_post)
-	lib.hook("camera-scopedof-post", _camera.on_scope_dof_post)
-	print("[likho] hooks registered")
+	var hooks: Array[int] = [
+		_register_hook(lib, "handling-weaponhandling", _handling.on_weapon_handling),
+		_register_hook(lib, "weaponrig-ammocheck", _weapon_rig.on_ammo_check),
+		_register_hook(lib, "weaponrig-ads-post", _weapon_rig.on_ads_post),
+		_register_hook(lib, "camera-scopedof-post", _camera.on_scope_dof_post)
+	]
+
+	var registered = hooks.filter(func(id): return id > -1)
+	if registered.size() == hooks.size():
+		print("[likho] all hooks registered")
+		return
+
+	print("[likho] mod registration failed, rolling back")
+	for id in registered:
+		lib.unhook(id)
+
+
+func _register_hook(lib, name: String, callback: Callable):
+	var id = lib.hook(name, callback)
+	if id != -1:
+		print("[likho] hook(%s):%s registered" % [name, id])
+	else:
+		push_error("[likho] hook(%s) failed" % name)
+	return id
+	
