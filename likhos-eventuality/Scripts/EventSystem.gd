e2a787cb982932512c3a9e4b79dbdd7ffa7e7f33
Ilia Gourianov <igourianov@gmail.com>
2026-04-30 09:53:48 -0400

    reworked roll bonuses to carry over to next map

--- likhos-eventuality/Scripts/EventSystem.gd
+++ likhos-eventuality/Scripts/EventSystem.gd
@@ -2,7 +2,8 @@ extends RefCounted
 
 var _lib
 
-const _EXCLUSIONS := {
+var rollBonuses: Dictionary = {}
+const blockers := {
 	"BTR": "Police",
 	"Police": "BTR",
 	"Airdrop": "CrashSite",
@@ -23,6 +24,9 @@ func on_activate_dynamic_event() -> void:
 
 
 func _activate_dynamic_event(es) -> void:
+
+	print("[likho] available events: %s" % [es.dynamicEvents.map(func(el): return el.function)])
+
 	if es.dynamicEvents.size() == 0:
 		return
 
@@ -30,37 +34,37 @@ func _activate_dynamic_event(es) -> void:
 	var events = es.dynamicEvents.duplicate()
 	events.shuffle()
 
-	var rollbonus: float = 0.0
 	for e in events:
-		var partner = _EXCLUSIONS.get(e.function, null)
-		if partner != null && activated.has(partner):
-			rollbonus += e.possibility * 0.5
-			print("[likho] event skipped: %s | Exclusive with: %s | Next roll bonus: +%s" % [e.name, partner, rollbonus])
+		var blocker: String = blockers.get(e.function, null)
+		var bonus: float = rollBonuses.get(e.function, 0.0)
+
+		if blocker != null && activated.has(blocker):
+			bonus += e.possibility
+			rollBonuses.set(e.function, bonus)
+			print("[likho] event skipped: %s | Blocked by: %s | Next roll bonus: +%s" % [e.name, blocker, bonus])
 			continue
 
-		var threshold: float = e.possibility + rollbonus
+		var threshold: float = e.possibility + bonus
 		var roll = randi_range(1, 100)
+		rollBonuses.set(e.function, 0.0)
 
 		if roll > threshold:
 			print("[likho] event missed: %s | Roll: %s/%s" % [e.name, roll, threshold])
 			continue
 
-		rollbonus = 0.0
 		activated[e.function] = true
 
 		if e.instant:
-			print("[likho] event activated: %s" % e.name)
+			print("[likho] event activated: %s | Roll: %s/%s" % [e.name, roll, threshold])
 			Callable(es, e.function).call()
 		else:
-			_delayed_call(es, e)
+			var delay = randi_range(30, 300)
+			print("[likho] event activated: %s | Roll: %s/%s | Delay: %02d:%02d" % [e.name, roll, threshold, int(floor(delay / 60.0)), delay % 60])
+			_activate_delayed_event(es, delay, e.function) # no await on purpose
 
-
-func _delayed_call(es, event) -> void:
-	var delay = randi_range(30, 300)
-	print("[likho] event activated: %s | Delay: %02d:%02d" % [event.name, int(floor(delay / 60.0)), delay % 60])
+func _activate_delayed_event(es, delay, name):
 	await es.get_tree().create_timer(delay, false).timeout
-	Callable(es, event.function).call()
-
+	Callable(es, name).call()
 
 func on_fighter_jet_post() -> void:
 	var es = _lib._caller
