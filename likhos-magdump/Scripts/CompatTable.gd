357057a57fb3a845364dfb370221a65f495df4fe
Ilia Gourianov <igourianov@gmail.com>
2026-04-30 22:11:56 -0400

    populated compat table

--- likhos-magdump/Scripts/CompatTable.gd
+++ likhos-magdump/Scripts/CompatTable.gd
@@ -15,4 +15,28 @@ const COMPAT := {
 	"res://Items/Weapons/AKS-74U/AKS-74U.tres": {
 		"res://Items/Weapons/AK-12/AK-12_Magazine.tres": {},
 	},
+	"res://Items/Weapons/RK-62/RK-62.tres": {
+		"res://Items/Weapons/AKM/AKM_Magazine.tres": {},
+	},
+	"res://Items/Weapons/RK-62/RK-62M.tres": {
+		"res://Items/Weapons/AKM/AKM_Magazine.tres": {},
+	},
+	"res://Items/Weapons/RK-95/RK-95.tres": {
+		"res://Items/Weapons/AKM/AKM_Magazine.tres": {},
+	},
+	"res://Items/Weapons/AKM/AKM.tres": {
+		"res://Items/Weapons/RK-62/RK_Magazine.tres": {},
+	},
+	"res://Items/Weapons/HK416/HK416.tres": {
+		"res://Items/Weapons/KAR-21/KAR-21_223_Magazine.tres": {},
+	},
+	"res://Items/Weapons/M4A1/M4A1.tres": {
+		"res://Items/Weapons/KAR-21/KAR-21_223_Magazine.tres": {},
+	},
+	"res://Items/Weapons/MK18/MK18.tres": {
+		"res://Items/Weapons/KAR-21/KAR-21_223_Magazine.tres": {},
+	},
+	"res://Items/Weapons/KAR-21/KAR-21_223.tres": {
+		"res://Items/Weapons/M4A1/STANAG_Magazine.tres": {},
+	},
 }
