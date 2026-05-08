4178f082fc44c93c7b493aea812cc91bc96128b9
Ilia Gourianov <igourianov@gmail.com>
2026-05-08 19:22:10 -0400

    movement speed and body stamina rework

--- likhos-weapon-handling-fixes/Scripts/Character.gd
+++ likhos-weapon-handling-fixes/Scripts/Character.gd
@@ -1,40 +1,44 @@
 extends RefCounted
 
 const ModConfig = preload("./ModConfig.gd")
+const STAMINA_RECOVERY: float = 30.0
+const STAMINA_RECOVERY_DELAY: float = 2
+const STAMINA_RUN: float = -20.0
+const STAMINA_SWIM: float = -40.0
+const STAMINA_HYDRATION_STRENGTH: float = 0.5
+
 
 var _lib
+var gameData = preload("res://Resources/GameData.tres")
+var _recovery_delay: float = 0.0
 
 
 func _init(lib) -> void:
 	_lib = lib
 
-
+# removes stamina drain on isInspecting
 func on_stamina(delta: float) -> void:
 	var c = _lib._caller
 	if c == null:
 		return
 	_lib.skip_super()
+	_leg_stamina(delta)
 
-	var gd = c.gameData
 
-	if gd.bodyStamina > 0 && (gd.isRunning || gd.overweight || (gd.isSwimming && gd.isMoving)):
-		if gd.overweight || gd.starvation || gd.dehydration:
-			gd.bodyStamina -= delta * 4.0
-		else:
-			gd.bodyStamina -= delta * 2.0
-	elif gd.bodyStamina < 100:
-		if gd.starvation || gd.dehydration:
-			gd.bodyStamina += delta * 5.0
-		else:
-			gd.bodyStamina += delta * 10.0
+func _leg_stamina(delta: float) -> void:
+	var h_factor: float = (gameData.hydration - 50.0) / 50.0
+	var stamina: float = 0.0
+	if gameData.isSwimming && gameData.isMoving:
+		stamina = STAMINA_SWIM * (1.0 - h_factor * STAMINA_HYDRATION_STRENGTH)
+		_recovery_delay = 0.0
+	elif gameData.isRunning:
+		stamina = STAMINA_RUN * (1.0 - h_factor * STAMINA_HYDRATION_STRENGTH)
+		_recovery_delay = 0.0
+	elif _recovery_delay >= STAMINA_RECOVERY_DELAY:
+		stamina = STAMINA_RECOVERY * (1.0 + h_factor * STAMINA_HYDRATION_STRENGTH)
+	else:
+		_recovery_delay += delta
 
-	if gd.armStamina > 0 && ((gd.primary || gd.secondary) && (gd.weaponPosition == 2 || gd.isAiming || gd.isCanted || gd.overweight) || (gd.isSwimming && gd.isMoving)):
-		if gd.overweight || gd.starvation || gd.dehydration:
-			gd.armStamina -= delta * 4.0
-		else:
-			gd.armStamina -= delta * 2.0
-	elif gd.armStamina < 100:
-		if gd.starvation || gd.dehydration:
-			gd.armStamina += delta * 10.0
-		else:
-			gd.armStamina += delta * 20.0
+	gameData.bodyStamina = clampf(gameData.bodyStamina + delta * stamina, 0.0, 100.0)
+
+
--- likhos-weapon-handling-fixes/Scripts/Controller.gd
+++ likhos-weapon-handling-fixes/Scripts/Controller.gd
@@ -2,47 +2,83 @@ extends RefCounted
 
 const ModConfig = preload("./ModConfig.gd")
 const Out = preload("../Lib/Out.gd")
+
 var gameData = preload("res://Resources/GameData.tres")
-
 var _lib
-var _weapon_rig
 
 
-func _init(lib, weapon_rig) -> void:
+func _init(lib) -> void:
 	_lib = lib
-	_weapon_rig = weapon_rig
 
 
-func on_movement_states_pre(_delta: float) -> void:
+func on_movement_states(delta: float) -> void:
 	var ctrl = _lib._caller
 	if ctrl == null:
 		return
+	_lib.skip_super()
 
-	if ModConfig.override_movement_speeds:
-		ctrl.crouchSpeed = ModConfig.crouch_speed
-		ctrl.walkSpeed = ModConfig.walk_speed
-		ctrl.sprintSpeed = ModConfig.sprint_speed
-
+	_update_state(ctrl)
+	_apply_speed(ctrl, delta)
 
 
-func on_movement_states_post(_delta: float) -> void:
-	var ctrl = _lib._caller
-	if ctrl == null:
+func _update_state(ctrl) -> void:
+	gameData.isIdle = !gameData.isMoving
+	gameData.isMoving = ctrl.inputDirection != Vector2.ZERO
+
+	if !gameData.isMoving:
+		gameData.isWalking = false
+		gameData.isRunning = false
 		return
 
-	if gameData.isWalking && gameData.isAiming && gameData.isScoped && ModConfig.current_scope_mag >= 2.0:
-		ctrl.currentSpeed = ctrl.walkSpeed * ModConfig.walk_scope_mult
-	elif gameData.isWalking && gameData.isAiming:
-		ctrl.currentSpeed = ctrl.walkSpeed * ModConfig.walk_aim_mult
-	elif gameData.isWalking && gameData.isCanted:
-		ctrl.currentSpeed = ctrl.walkSpeed * ModConfig.walk_cant_mult
+	if gameData.bodyStamina <= 0.0 || gameData.overweight || gameData.fracture:
+		gameData.isRunning = false
+	elif gameData.sprintMode == 1:
+		gameData.isRunning = Input.is_action_pressed("sprint")
+	elif Input.is_action_just_pressed("sprint"):
+		gameData.isRunning = !gameData.isRunning
+
+	gameData.isWalking = !gameData.isRunning
+	if gameData.isRunning:
+		gameData.isCrouching = false
+
+
+func _apply_speed(ctrl, delta: float) -> void:
+	var target: float
+	var rate: float
+	var crouchSpeed: float = ctrl.crouchSpeed if !ModConfig.override_movement_speeds else ModConfig.crouch_speed
+	var walkSpeed: float = ctrl.walkSpeed if !ModConfig.override_movement_speeds else ModConfig.walk_speed
+	var sprintSpeed: float = ctrl.sprintSpeed if !ModConfig.override_movement_speeds else ModConfig.sprint_speed
+
+	if !gameData.isMoving:
+		target = 0.0
+		rate = 5.0
+	elif gameData.isCrouching:
+		target = crouchSpeed
+		rate = 2.5
+	elif gameData.isRunning:
+		target = sprintSpeed
+		rate = 1.0
+	elif gameData.isAiming && gameData.isScoped && ModConfig.current_scope_mag >= 2.0:
+		target = walkSpeed * ModConfig.walk_scope_mult
+		rate = 2.5
+	elif gameData.isAiming:
+		target = walkSpeed * ModConfig.walk_aim_mult
+		rate = 2.5
+	elif gameData.isCanted:
+		target = walkSpeed * ModConfig.walk_cant_mult
+		rate = 2.5
+	else:
+		target = ctrl.walkSpeed
+		rate = 2.5
+
+	ctrl.currentSpeed = lerp(ctrl.currentSpeed, target, delta * rate)
 
 
 func on_input(evt) -> void:
 	var ctrl = _lib._caller
 	if ctrl == null:
 		return
-	
+
 	if evt is InputEventMouseMotion:
 		_lib.skip_super()
 		_mouse_input(ctrl, evt)
