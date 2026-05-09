58ce640282e8dea9b446743298ed3a2f5659344e
Ilia Gourianov <igourianov@gmail.com>
2026-05-09 15:52:04 -0400

    stamina math polish

--- likhos-weapon-handling-fixes/Scripts/Character.gd
+++ likhos-weapon-handling-fixes/Scripts/Character.gd
@@ -3,25 +3,20 @@ extends RefCounted
 const ModConfig = preload("./ModConfig.gd")
 const Out = preload("../Lib/Out.gd")
 
-const STAMINA_VITAL_BREAKPOINT: float = 50.0
-const STAMINA_VITAL_WEIGHT: float = 1.0
-
-const STAMINA_RECOVERY: float = 40.0
-const STAMINA_RECOVERY_DELAY: float = 1
-const STAMINA_RECOVERY_DELAY_EMPTY: float = 2.5
+const STAMINA_RECOVERY: float = 100.0
+const STAMINA_RECOVERY_DELAY: float = 2.0
+const STAMINA_RECOVERY_DELAY_EMPTY: float = 5.0
 const STAMINA_RECOVERY_DELAY_MIN: float = 0.2
 
-const BODY_STAMINA_RUN: float = -20.0
-const BODY_STAMINA_SWIM: float = -30.0
-const BODY_STAMINA_WEIGHT_BASE: float = 0.1
-const BODY_STAMINA_WEIGHT_MOD: float = 1.2
+const BODY_STAMINA_RUN: float = -15.0
+const BODY_STAMINA_SWIM: float = -20.0
+const BODY_STAMINA_FACTOR_MIN: float = 0.2
 
-const ARM_STAMINA_CANTED: float = -5.0
-const ARM_STAMINA_AIM: float = -8.0
-const ARM_STAMINA_AIM_ZOOM: float = -12.0
-const ARM_STAMINA_RAISED: float = -3.0
+const ARM_STAMINA_CANTED: float = -2.0
+const ARM_STAMINA_AIM: float = -3.0
+const ARM_STAMINA_AIM_ZOOM: float = -3.5
+const ARM_STAMINA_RAISED: float = -2.0
 const ARM_STAMINA_AIM_CROUCH_MOD: float = 0.5
-const ARM_STAMINA_WEIGHT_MOD: float = 0.5
 
 
 var _lib
@@ -47,31 +42,28 @@ func on_stamina(delta: float) -> void:
 
 func _body_stamina(delta: float, current_inv_weight: float, max_inv_weight: float) -> void:
 	var stamina: float = 0.0
-	var weight_factor: float = BODY_STAMINA_WEIGHT_BASE + (current_inv_weight / max_inv_weight) * BODY_STAMINA_WEIGHT_MOD
+	var weight_factor: float = maxf(BODY_STAMINA_FACTOR_MIN, current_inv_weight / max_inv_weight)
 	if gameData.isSwimming && gameData.isMoving:
 		stamina = BODY_STAMINA_SWIM * weight_factor
 		_body_recovery_delay = 0.0
 	elif gameData.isRunning:
 		stamina = BODY_STAMINA_RUN * weight_factor
 		_body_recovery_delay = 0.0
+	elif gameData.bodyStamina >= 100.0:
+		return
 	else:
-		if gameData.bodyStamina >= 100.0:
-			return
-		var hydration_factor: float = _vital_factor(gameData.hydration)
-		var delay_base: float = STAMINA_RECOVERY_DELAY_EMPTY if gameData.bodyStamina <= 0.0 else STAMINA_RECOVERY_DELAY
-		var delay_threshold: float = maxf(delay_base * (1.0 - hydration_factor), STAMINA_RECOVERY_DELAY_MIN)
-		if _body_recovery_delay >= delay_threshold:
-			stamina = STAMINA_RECOVERY * (1.0 + hydration_factor)
-		else:
+		var hydration_factor: float = gameData.hydration / 100.0
+		if _body_recovery_delay < _recovery_delay_threshold(gameData.bodyStamina, hydration_factor):
 			_body_recovery_delay += delta
 			return
+		stamina = STAMINA_RECOVERY * hydration_factor * hydration_factor
 
 	gameData.bodyStamina = clampf(gameData.bodyStamina + delta * stamina, 0.0, 100.0)
 
 
 func _arm_stamina(delta: float) -> void:
 	var stamina: float = 0.0
-	var weight_factor: float = (ModConfig.current_weapon_weight / ModConfig.BASE_WEAPON_WEIGHT) * ARM_STAMINA_WEIGHT_MOD
+	var weight_factor: float = ModConfig.current_weapon_weight / ModConfig.BASE_WEAPON_WEIGHT
 	if gameData.isCanted:
 		stamina = ARM_STAMINA_CANTED * weight_factor
 		_arm_recovery_delay = 0.0
@@ -82,23 +74,21 @@ func _arm_stamina(delta: float) -> void:
 			stamina *= ARM_STAMINA_AIM_CROUCH_MOD
 		_arm_recovery_delay = 0.0
 	elif gameData.weaponPosition == 2:
-		stamina = ARM_STAMINA_RAISED
+		stamina = ARM_STAMINA_RAISED * weight_factor
 		_arm_recovery_delay = 0.0
 	elif gameData.armStamina >= 100.0:
 		return
 	else:
-		var energy_factor: float = _vital_factor(gameData.energy)
-		var delay_base: float = STAMINA_RECOVERY_DELAY_EMPTY if gameData.armStamina <= 0.0 else STAMINA_RECOVERY_DELAY
-		var delay_threshold: float = maxf(delay_base * (1.0 - energy_factor), STAMINA_RECOVERY_DELAY_MIN)
-		if _arm_recovery_delay >= delay_threshold:
-			stamina = STAMINA_RECOVERY * (1.0 + energy_factor)
-		else:
+		var energy_factor: float = gameData.energy / 100.0
+		if _arm_recovery_delay < _recovery_delay_threshold(gameData.armStamina, energy_factor):
 			_arm_recovery_delay += delta
 			return
+		stamina = STAMINA_RECOVERY * energy_factor * energy_factor
 
 	gameData.armStamina = clampf(gameData.armStamina + delta * stamina, 0.0, 100.0)
 
 
-func _vital_factor(value: float) -> float:
-	return STAMINA_VITAL_WEIGHT * (value - STAMINA_VITAL_BREAKPOINT) / STAMINA_VITAL_BREAKPOINT
+func _recovery_delay_threshold(current_stamina: float, vital_factor: float) -> float:
+	var delay_base: float = STAMINA_RECOVERY_DELAY_EMPTY if current_stamina <= 0.0 else STAMINA_RECOVERY_DELAY
+	return maxf(STAMINA_RECOVERY_DELAY_MIN, delay_base * (1.0 - vital_factor))
 
