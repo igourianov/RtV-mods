f7b0c36b4012759ac3b889e29237de776f0051d5
Ilia Gourianov <igourianov@gmail.com>
2026-05-10 19:43:57 -0400

    fixed flashlight breaking after changing maps

--- likhos-weapon-handling-fixes/Scripts/Flashlight.gd
+++ likhos-weapon-handling-fixes/Scripts/Flashlight.gd
@@ -3,7 +3,7 @@ const Out = preload("../Lib/Out.gd")
 const FlashlightDriver = preload("./FlashlightDriver.gd")
 
 var _lib
-var _driver_created := false
+
 
 func _init(lib) -> void:
 	_lib = lib
@@ -17,8 +17,7 @@ func on_physics_process(delta: float) -> void:
 	if !caller.gameData.freeze && caller.gameData.flashlight:
 		caller.Consumption(delta)
 
-	if !_driver_created:
-		_driver_created = true
+	if !caller.has_node("FlashlightDriver"):
 		var driver = FlashlightDriver.new()
 		driver.name = "FlashlightDriver"
 		caller.add_child(driver)
