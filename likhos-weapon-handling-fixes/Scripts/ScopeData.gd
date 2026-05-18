da0e00b9a437bad649233891dcc3243cc4660438
Ilia Gourianov <igourianov@gmail.com>
2026-05-18 13:34:14 -0400

    removed dead code

--- likhos-weapon-handling-fixes/Scripts/ScopeData.gd
+++
@@ -1,6 +0,0 @@
-extends "res://Scripts/AttachmentData.gd"
-class_name ScopeData
-
-@export_group("Scope Parameters")
-@export var reticlePlane: StringName = null
-@export var zoomRange: Array[float] = []
