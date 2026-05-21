6dd6bfa0d4dd9725173f55e60e8cecc7329eb4ff
Ilia Gourianov <igourianov@gmail.com>
2026-05-21 10:39:04 -0400

    true up weights and renaming

--- likhos-tag/Scripts/Catalog.gd
+++ likhos-tag/Scripts/Catalog.gd
@@ -6,55 +6,56 @@ const DATA := {
 	"ACOG": {
 		"display": "ACOG",
 		"name": "Trijicon ACOG TA31",
-		"weight": 0.45,
+		"weight": 0.5,
 	},
 	"HMR": {
 		"display": "HAMR",
 		"name": "Leupold HAMR 4x",
-		"weight": 0.4,
+		"weight": 0.5,
 	},
 	"POSP": {
+		"name": "Зенит ПОСП 2-6х"
 		"weight": 0.9,
 	},
 	"PU": {
 		"display": "PU",
-		"name": "PU",
-		"weight": 0.75,
+		"name": "ПУ 3.5x",
+		"weight": 0.4,
 	},
 	"Vudu": {
 		"display": "Vudu",
-		"name": "EOTech Vudu 1-10x FFP",
-		"weight": 0.8,
+		"name": "EOTech Vudu",
+		"weight": 0.75,
 	},
 	"Leopard": {
-		"display": "Leupold",
+		"display": "Mark 8",
 		"name": "Leupold Mark 8 CQBSS",
 		"weight": 0.85,
 	},
 	"RMR": {
 		"display": "RMR",
 		"name": "Trijicon RMR",
-		"weight": 0.1,
+		"weight": 0.035,
 	},
 	"EXPS": {
-		"display": "EXP",
+		"display": "EXPS",
 		"name": "EOTech EXPS",
-		"weight": 0.5,
+		"weight": 0.4,
 	},
 	"Kobra": {
 		"display": "Kobra",
-		"name": "Kobra",
+		"name": "Аксион ЭКП-8-16 \"Кобра\"",
 		"weight": 0.4,
 	},
 	"Micro": {
 		"display": "T2",
 		"name": "Aimpoint T2",
-		"weight": 0.15,
+		"weight": 0.2,
 	},
 	"MRO": {
 		"display": "MRO",
 		"name": "Trijicon MRO",
-		"weight": 0.15,
+		"weight": 0.2,
 	},
 	"PRO": {
 		"display": "PRO",
@@ -64,7 +65,7 @@ const DATA := {
 	"SRO": {
 		"display": "SRO",
 		"name": "Trijicon SRO",
-		"weight": 0.1,
+		"weight": 0.045,
 	},
 }
 
