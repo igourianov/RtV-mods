c255d0a1b6c2773a01f8286e22f9ed1422306d9c
Ilia Gourianov <igourianov@gmail.com>
2026-05-24 09:21:41 -0400

    refactored tooltip expansion

--- likhos-tag/Scripts/Tooltip.gd
+++ likhos-tag/Scripts/Tooltip.gd
@@ -2,6 +2,7 @@ extends RefCounted
 
 const Out = preload("../Lib/Out.gd")
 const Catalog = preload("./Catalog.gd")
+const ROW_PATH = "Panel/Margin/Elements/"
 
 var _lib
 
@@ -13,13 +14,13 @@ func _init(lib) -> void:
 func on_ready_pre():
 	var tooltip = _lib._caller
 	_create_row(tooltip, "likho_model", "Model:")
-	#_create_row(tooltip, "likho_grau", "GRAU index:")
+	_create_row(tooltip, "likho_grau", "GRAU index:")
 
 
 func on_reset_post():
 	var tooltip = _lib._caller
-	var row = tooltip.get_meta("likho_model")
-	row.hide()
+	tooltip.get_node(ROW_PATH + "likho_model").hide()
+	tooltip.get_node(ROW_PATH + "likho_grau").hide()
 
 
 func on_update_post(item) -> void:
@@ -34,6 +35,7 @@ func on_update_post(item) -> void:
 	var parts: Array[String] = []
 	var has_armor := false
 	var caliber_name: String
+	var extra_data = Catalog.DATA.get(itemData.file, {})
 
 	tooltip.type.text = itemData.subtype if itemData.type == "Attachment" && itemData.subtype else itemData.type
 
@@ -54,37 +56,37 @@ func on_update_post(item) -> void:
 	else:
 		tooltip.subpanel.hide()
 
-	if caliber_name || itemData.caliber:
-		tooltip.caliber.get_child(0).text = caliber_name if caliber_name else itemData.caliber
+	if caliber_name:
+		tooltip.caliber.get_child(0).text = caliber_name
 		tooltip.caliber.show()
-	else:
-		tooltip.caliber.hide()
 
-	if itemData.maxAmount || itemData.capacity:
-		tooltip.capacity.get_child(0).text = itemData.maxAmount if itemData.maxAmount else "+%dkg" % itemData.capacity
+	if itemData.maxAmount:
+		tooltip.capacity.get_child(0).text = itemData.maxAmount
 		tooltip.capacity.show()
-	else:
-		tooltip.capacity.hide()
 
-	var extra_data = Catalog.DATA.get(itemData.file, {})
 	var grau_index = extra_data.get("grau_index", "")
+	if grau_index:
+		var row = tooltip.get_node(ROW_PATH + "likho_grau")
+		row.get_child(1).text = grau_index
+		row.show()
+
 	var model_name = extra_data.get("model", "")
-	var model_row = tooltip.get_meta("likho_model", null)
-	if model_row && (grau_index || model_name):
-		model_row.get_child(0).text = "GRAU index:" if grau_index else "Model:"
-		model_row.get_child(1).text = grau_index if grau_index else model_name
-		model_row.show()
+	if model_name:
+		var row = tooltip.get_node(ROW_PATH + "likho_model")
+		row.get_child(1).text = model_name
+		row.show()
 
 	tooltip.panel.size = Vector2(256, 0)
 	tooltip.interface.tooltipOffset = tooltip.panel.size.y / 2.0
 
 
-func _create_row(tooltip, meta_key: String, title_text: String) -> Control:
+func _create_row(tooltip, node_name: String, title_text: String) -> Control:
 
 	var sibling: Label = tooltip.weight
 	var parent: Node = sibling.get_parent()
 
 	var row := HBoxContainer.new()
+	row.name = node_name
 	row.add_theme_constant_override("separation", 4)
 
 	var title := Label.new()
@@ -101,5 +103,4 @@ func _create_row(tooltip, meta_key: String, title_text: String) -> Control:
 	row.add_child(value)
 
 	parent.add_child(row)
-	tooltip.set_meta(meta_key, row)
 	return row
