719ed238ed4f097de14e1d3ae686d0eb61df8f82
Ilia Gourianov <igourianov@gmail.com>
2026-06-17 08:57:35 -0400

    fixed occassional flashlight double click sound

--- likhos-weapon-handling-fixes/Scripts/Audio/AudioChunkPlayer.gd
+++ likhos-weapon-handling-fixes/Scripts/Audio/AudioChunkPlayer.gd
@@ -1,29 +1,21 @@
 extends AudioStreamPlayer
 
-var _duration := 0.0
-
 
 func _init(audio_stream: AudioStream = null, volume := 0.0) -> void:
 	stream = audio_stream
 	volume_db = volume
 	max_polyphony = 10
-	set_process(false)
 
 
 func play_chunk(start := 0.0, duration := 0.0) -> void:
 	if !stream:
 		return
 	play(start)
-	if duration > 0.0:
-		_duration = duration
-		set_process(true)
-	else:
-		set_process(false)
+	if duration <= 0.0:
+		return
+	get_tree().create_timer(duration).timeout.connect(_stop_playback.bind(get_stream_playback()))
 
 
-func _process(delta: float) -> void:
-	_duration -= delta
-	if _duration <= 0.0:
-		set_process(false)
-		if playing:
-			stop()
+func _stop_playback(playback: AudioStreamPlayback) -> void:
+	if playback && playback.is_playing():
+		playback.stop()
