29ca16a35c23b55ef2416235cd2d4360b2b65653
Ilia Gourianov <igourianov@gmail.com>
2026-06-26 09:44:58 -0400

    wired in static setting into MCM

--- likhos-radiola/Scripts/Main.gd
+++ likhos-radiola/Scripts/Main.gd
@@ -15,8 +15,16 @@ func setup(lib) -> void:
 
 
 func create_config(config: ConfigFile) -> void:
-	config.set_value("audio", "static_enabled", true)
+	config.set_value("Category", "General", { "menu_pos": 0 })
+	config.set_value("Bool", "static_enabled", {
+		"name": "Radio static",
+		"tooltip": "Play the radio hiss/static bed underneath the music",
+		"default": true,
+		"value": true,
+		"menu_pos": 1,
+		"category": "General"
+	})
 
 
 func load_config(config: ConfigFile) -> void:
-	_static_enabled = bool(config.get_value("audio", "static_enabled", true))
+	_static_enabled = bool(config.get_value("Bool", "static_enabled", {}).get("value", true))
